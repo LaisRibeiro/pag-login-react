c03e9017d6f4dffb5fc9da6650611d5fa5e62b0f
LaisRibeiro <riibeirolais@gmail.com>
2021-01-24 23:34:45 -0300

    fix: add route view TelaRegister

--- pag-login-react/src/routes/index.js
+++ pag-login-react/src/routes/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import TelaLogin from '../views/TelaLogin';
+import TelaRegister from '../views/TelaRegister';
 
 export default function Routes() {
     return (
       <Router>
         <Switch>
             <Route exact path="/" component={TelaLogin}/>
+            <Route path="/register-user" component={TelaRegister}/>
         </Switch>
     </Router>
   );
